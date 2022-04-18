c10dd4adc3b0342f8623fea48a1a28066d3419b6
seohyoju <kinnylovemy@gmail.com>
2022-04-18 17:36:46 +0900

    simple_login git init

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:fcm_push_test/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          titleTextStyle: TextStyle(color: Colors.black),
+        ),
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(),
+    );
+  }
+}
